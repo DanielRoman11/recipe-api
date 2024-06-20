c343772a5bf025cdf7aa2d5025c0cda430e3e402
Daniel Román <danielroman.05111@gmail.com>
2024-06-20 15:24:57 -0500

    feat: MySQL database setup

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +80,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'ENGINE': 'django.db.backends.mysql',
+      'HOST': os.environ.get('DB_HOST'),
+      'NAME': os.environ.get('DB_NAME'),
+      'PORT': os.environ.get('DB_PORT'),
+      'USER': os.environ.get('DB_USER'),
+      'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
