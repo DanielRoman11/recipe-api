1ec0754a875957b051a40be0dca9ea18d20efaa8
Daniel Román <danielroman.05111@gmail.com>
2024-07-04 17:41:43 -0500

    bug: Recipe model wasnt loaded into Github

--- app/core/models.py
+++ app/core/models.py
@@ -9,6 +9,8 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+from django.conf import settings
+
 
 class UserManager(BaseUserManager):
     """Manager for users."""
@@ -45,3 +47,26 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
+
+
+class Recipe(models.Model):
+    """Model definition for Recipe."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+    )
+    link = models.CharField(
+        max_length=255,
+        blank=True,
+    )
+
+    def __str__(self):
+        """Unicode representation of Recipe."""
+        return self.title
